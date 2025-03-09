905f0c3fa260c52eca9d2b3f5a1c6bae7185c625
unknown <swati manikeri>
2025-03-09 20:29:33 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Test
+{
+public static void main(String[] args){
+  int[] arr={4,6,7,8,9};
+  bubble(arr);
+  System.out.println(Arrays.toString(arr));
+}
+static void bubble(int[] arr){
+    for(int i=0;i<arr.length;i++){
+        for(int j=1;j<arr.length-i;j++){
+            if(arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+            }
+        }
+    }
+}
+}
