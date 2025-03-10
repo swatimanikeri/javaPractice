ade1e61e01484d6d62f1dce97a0f3dcb0c27a081
swatimanikeri <manikeriswati@gmail.com>
2025-03-10 19:36:27 +0530

    Added binary search and updated selection sort

---
+++ binarySearch.java
@@ -0,0 +1,26 @@
+
+public class binarySearch{
+
+    public static void main(String[] args){
+    int[] arr={1,2,4,5,7,8,9};
+    int target=8;
+    System.out.println(binary(arr,target));
+    }
+    
+    static int binary(int[] arr,int target){
+      int start=0;
+      int end=arr.length-1;
+    
+      
+      while(start<=end){
+        int mid=start+(end-start)/2;
+        if(target<arr[mid]){
+        end=mid-1;
+      }
+       else if(target>arr[mid]){
+        start=mid+1;
+      }else{
+        return mid;
+      }
+    
+    }return -1;}}
