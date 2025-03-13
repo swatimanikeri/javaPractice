a2257d3ec9f1ce0455dfd6bb2d08a0f85058f99d
swatimanikeri <manikeriswati@gmail.com>
2025-03-13 21:56:03 +0530

    Initial commit

---
+++ MultiThreadingExample.java
@@ -0,0 +1,48 @@
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
+
+public class MultiThreadingExample {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        thread1.setName("Thread-1");
+        thread1.start();
+
+        Thread thread2 = new Thread(new MyRunnable());
+        thread2.setName("Thread-2");
+        thread2.start();
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
