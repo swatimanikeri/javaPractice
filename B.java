3b891d1636545e167be9aec2cf65cc9f272e248e
swatimanikeri <manikeriswati@gmail.com>
2025-03-28 22:16:43 +0530

    tweth commit

---
+++ B.java
@@ -0,0 +1,11 @@
+
+
+public class B {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I am tired");
+        }
+       
+    }
+    
+}
