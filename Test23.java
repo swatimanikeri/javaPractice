b098cfb196bb8d40f425b84e15d4073dcf7a63f9
swatimanikeri <manikeriswati@gmail.com>
2025-04-10 23:18:34 +0530

    twentytwo commit

---
+++ Test23.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Test23 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("tommy");
+        list.add("cat");
+        list.add("school");
+        list.add("table");
+      
+        for (String item : list) {
+            System.out.println(item);
+        }
+
+
+
+    }
+    
+
+
+
+
+}
