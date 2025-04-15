9809ca38a921e35b32603d8ff883168a3b994ea2
swatimanikeri <manikeriswati@gmail.com>
2025-04-15 21:46:51 +0530

    twentySeven  commit

---
+++ Test28.java
@@ -0,0 +1,10 @@
+public public class Test28 {
+    public static void main(String[] args) {
+        System.out.println("This is  1 day before Exam
+        
+        ");
+    }
+}
+ {
+    
+}
---
+++ Test29.java
@@ -0,0 +1,5 @@
+public class Test29 {
+    public static void main(String[] args) {
+        System.out.println("i think this is one month from where i started ,I started");
+    }
+}
