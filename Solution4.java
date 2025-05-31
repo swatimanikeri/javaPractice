247bb736d71dcacc1a2453abd01ffd3267f98cf8
swatimanikeri <manikeriswati@gmail.com>
2025-05-31 23:00:14 +0530

    Create Solution4.java

---
+++ Solution4.java
@@ -0,0 +1,38 @@
+class Solution4 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            // Step 2: Find the element just greater than nums[i]
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+
+            // Step 3: Swap nums[i] and nums[j]
+            swap(nums, i, j);
+        }
+
+        // Step 4: Reverse the suffix starting from i+1
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
