86972404c537bb0f12294ea12340862e0f52bbc9
swatimanikeri <manikeriswati@gmail.com>
2025-06-01 22:21:06 +0530

    Create SolutionGCD .java

---
+++ SolutionGCD .java
@@ -0,0 +1,12 @@
+class SolutionGCD {
+    public int GCD(int n1, int n2) {
+        while (n1 > 0 && n2 > 0) {
+            if (n1 > n2)
+                n1 = n1 % n2;
+            else
+                n2 = n2 % n1;
+        }
+
+        return (n1 == 0) ? n2 : n1;
+    }
+}
