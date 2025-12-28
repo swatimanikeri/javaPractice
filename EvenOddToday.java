1cd8fdf4b69976f74d7b34ad88aca33c33e632fe
swatimanikeri <manikeriswati@gmail.com>
2025-12-28 23:00:15 +0530

    Rename class from EvenOdd to EvenOddToday

---
+++ EvenOddToday.java
@@ -0,0 +1,16 @@
+class EvenOddToday{
+public static void main(String[] args){
+
+    if (isEven(35)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+}
+public boolean isEven(int n){
+if(n%2==0){
+return true;
+}
+returnn false;
+
+}
