6312dd33010459d42601cacc6e2a33fee0e0eb77
swatimanikeri <manikeriswati@gmail.com>
2026-01-04 22:26:05 +0530

    Add EvenOdds class to check even or odd numbers

---
+++ EvenOdds.java
@@ -0,0 +1,11 @@
+class EvenOdds {
+    public static void main(String[] args) {
+        int n = 35;
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
